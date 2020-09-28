81407f8d00ded22d3802d6fb51390e026a84fc8c
“Toshiyuki023Hori“ <World88Toshi@gmail.com>
2020-09-27 17:06:41 -0700

    [fix]ChoosingUserが認識されない
    modelsのfieldをchoosing_userに変更。厳密に言えばmigrate直前。

--- frontend/src/containers/Organisms/User_Add_PickUp_Form.jsx
+++ frontend/src/containers/Organisms/User_Add_PickUp_Form.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import SmallButton from '../../presentational/shared/SmallButton';
 import styled from 'styled-components';
 import history from '../../history';
 import axios from 'axios';
 import { CircularProgress } from '@material-ui/core';
+import SmallButton from '../../presentational/shared/SmallButton';
 
 class User_Add_PickUp_Form extends Component {
   constructor(props) {
@@ -36,14 +36,13 @@ class User_Add_PickUp_Form extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.lines != this.state.lines) {
-      console.log("FIRE!!")
+      console.log('FIRE!!');
       axios
         .get('http://express.heartrails.com/api/json?method=getStations&line=' + this.state.lines)
         .then((res) => {
-          res.data.response.station.map((station) => {
-            this.setState({allStations :[...this.state.allStations, station.name]})
-          })
-        });
+          this.setState({ allStations: res.data.response.station });
+        })
+        .catch((err) => console.log(err));
     }
   }
 
@@ -51,8 +50,30 @@ class User_Add_PickUp_Form extends Component {
     const name = e.target.name;
     const value = e.target.value;
 
-    // console.log("Change is " + this.state.allStations)
     this.spreadAndSetState(name, value);
+  };
+
+  handleSubmit = () => {
+    const { owner, lines, stations, textInput, allLines, allStations } = this.state;
+    const { axiosUrl } = this.props;
+    const token = localStorage.getItem('token');
+    const authHeader = {
+      headers: {
+        Authorization: 'Token ' + token,
+      },
+    };
+    const pickupPlaces = lines + ' ' + stations + '駅';
+    axios
+      .post(
+        axiosUrl + 'pickup/',
+        {
+          name: pickupPlaces,
+          choosingUser: owner.id,
+        },
+        authHeader
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -63,23 +84,36 @@ class User_Add_PickUp_Form extends Component {
     } else {
       return (
         <div>
-          <h2>ピックアップ地点追加</h2>
-          <select name="lines" onChange={this.handleChange}>
-            <option value="">路線を選んでください</option>
-            {allLines.map((line) => {
-              return <option value={line}>{line}</option>;
-            })}
-          </select>
-          <select name="stations" onChange={this.handleChange}>
-            <option value="">駅を選んでください</option>
-            {
-            allStations != '' &&
-            allStations.map((line) => {
-              // console.log(allStations)
-            return <option value={line}>{line}</option>;
-            })
-            }
-          </select>
+          <div>
+            <h2>ピックアップ地点追加</h2>
+            <select name="lines" onChange={this.handleChange}>
+              <option value="">路線を選んでください</option>
+              {allLines.map((line, idx) => {
+                return (
+                  <option key={idx} value={line}>
+                    {line}
+                  </option>
+                );
+              })}
+            </select>
+            <select name="stations" onChange={this.handleChange}>
+              <option value="">駅を選んでください</option>
+              {allStations != '' &&
+                allStations.map((line, idx) => {
+                  // console.log(allStations)
+                  return (
+                    <option key={idx} value={line.name}>
+                      {line.name}
+                    </option>
+                  );
+                })}
+            </select>
+          </div>
+          <div>
+            <label>その他の地点をご希望の場合は直接ご記入ください(バス停など)</label>
+            <input name="textInput" type="text" value={textInput} onChange={this.handleChange} />
+          </div>
+          <SmallButton btn_name="追加" btn_type="submit" btn_click={this.handleSubmit} />
         </div>
       );
     }
