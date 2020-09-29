b9d60fd174a1a2946ad4a77fbb4a80d29cb32e5e
“Toshiyuki023Hori“ <World88Toshi@gmail.com>
2020-09-29 16:22:47 -0700

    [Update] Give_Item詳細画面情報表示成功
    Give_Item_Descriptionにて初回レンダー時に情報表示成功。

--- frontend/src/containers/Organisms/Give_Item_Description.jsx
+++ frontend/src/containers/Organisms/Give_Item_Description.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import history from '../../history';
+import {CircularProgress, Chip} from '@material-ui/core';
 
 class Give_Item_Description extends Component {
   constructor(props) {
@@ -15,44 +16,86 @@ class Give_Item_Description extends Component {
   }
 
   setDataToState = (key, value) => {
-    this.setState({[key] : value})
+    this.setState({ [key]: value });
   };
 
-  componentDidMount() {
+  spreadDataToObject = (spreadKey, key, value) => {
+    this.setState({ [spreadKey]: { ...this.state[spreadKey], [key]: value } });
+  };
+
+  spreadDataToArray = (spreadKey, value) => {
+    this.setState({ [spreadKey]: [...this.state[spreadKey], value] });
+  };
+
+  async componentDidMount() {
     const { axiosUrl, loginUser } = this.props;
-    const { parentItem, giveItem, pickups } = this.state;
     const parent_id = parseInt(this.props.parent_id);
 
-
-    axios
+    await axios
       .all([
         axios.get(axiosUrl + 'parent/' + parent_id),
         axios.get(axiosUrl + 'giveitem/?parent_item=' + parent_id),
       ])
       .then(
         axios.spread((resParent, resGive) => {
-          this.setDataToState("parentItem", resParent.data);
-          this.setDataToState("giveItem", resGive.data[0]);
+          this.setDataToState('parentItem', resParent.data);
+          this.setDataToState('giveItem', resGive.data[0]);
         })
       )
       .catch((err) => console.log(err));
-  }
 
-  componentDidUpdate(prevProps, prevState){
-    const {axiosUrl, loginUser} = this.props;
-    const {parentItem, giveItem, pickups} = this.state;
+    axios.get(axiosUrl + 'category/' + this.state.giveItem.category).then((res) => {
+      this.spreadDataToObject('giveItem', 'category', res.data.name);
+    });
 
-    if(prevState.giveItem !== this.state.giveItem){
-        axios.get(axiosUrl + 'category/' + this.state.giveItem.category)
-        .then((res) => {
-            console.log(res.data)
-            this.setState({parentItem : {...parentItem, category:res.data.name}})
-        });
+    if (this.state.parentItem.bland !== null) {
+      axios.get(axiosUrl + 'bland/' + this.state.parentItem.bland).then((res) => {
+        console.log(res.data.name);
+        this.spreadDataToObject('parentItem', 'bland', res.data.name);
+      });
+    } else if (this.state.parentItem.bland === null) {
+      console.log('Else if');
+      this.spreadDataToObject('parentItem', 'bland', '無し');
     }
+
+    await axios
+      .all([
+        axios.get(axiosUrl + 'image/?item=' + this.state.giveItem.id),
+        axios.get(axiosUrl + 'pickup/?choosingUser=' + this.state.parentItem.owner),
+      ])
+      .then(
+        axios.spread((resImages, resPickups) => {
+          resImages.data.map((imageObj) => {
+            this.spreadDataToArray('images', imageObj.image);
+          });
+          resPickups.data.map((pickupObj) => {
+            this.spreadDataToArray('pickups', pickupObj.name);
+          });
+        })
+      );
   }
 
   render() {
-    return <h2>TEST</h2>;
+    const { parentItem, giveItem, pickups, images } = this.state;
+    if (images === []) {
+        return <CircularProgress/>
+    }
+    return (
+        <div>
+            <h1>{parentItem.name}</h1>
+            <p>{giveItem.createdAt}に投稿</p>
+            <p>状態 : {giveItem.state}</p>
+            <p>ブランド : {parentItem.bland}</p>
+            <p>{giveItem.detail}</p>
+            <p>{giveItem.category}</p>
+            <p>ピックアップ地点</p>
+            <ul>
+                {pickups.map((pickup) => {
+                    return <li>{pickup}</li>
+                })}
+            </ul>
+        </div>
+    ) 
   }
 }
 
