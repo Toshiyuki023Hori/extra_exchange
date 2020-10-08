b65668f0924490c564ce388805bf40a11cb59c3d
Toshi023 <World.88Toshi@gmail.com>
2020-10-08 16:36:33 -0700

    [Add] Deal_Detailファイル追加

---
+++ frontend/src/containers/Pages/Deal_Detail.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import styled from "styled-components"
+
+class Deal_Detail extends Component {
+ constructor(props){
+ super(props);
+}
+}
+
+export default Deal_Detail;
