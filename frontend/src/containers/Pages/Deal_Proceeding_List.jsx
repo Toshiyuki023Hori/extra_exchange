25a9c2413d9851f4d43753f207c743865cb7e781
“Toshiyuki023Hori“ <World88Toshi@gmail.com>
2020-10-07 17:58:03 -0700

    [Add] 進行中の取引一覧ページ追加 #150

---
+++ frontend/src/containers/Pages/Deal_Proceeding_List.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+class Deal_Proceeding_List extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default Deal_Proceeding_List;
