239b4e6974e10d6b112d93913783ef91d0ebed20
Toshi023 <World.88Toshi@gmail.com>
2020-10-22 19:45:15 +0900

    [Update]Give_Item_Add画像プレビューレイアウト完成 #176
    このコミット前にWant_Item_Addも完成済 #186

--- frontend/src/containers/Organisms/Give_Item_Add_Form.jsx
+++ frontend/src/containers/Organisms/Give_Item_Add_Form.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import history from '../../history';
+import styled from 'styled-components';
 import MiddleButton from '../../presentational/shared/MiddleButton';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import ValidationMessage from '../../presentational/shared/ValidationMessage';
+import {
+  mixinUlLabel,
+  mixinInputForm,
+  mixinLiTag,
+  mixinDropDown,
+  mixinTextArea,
+} from '../../presentational/shared/static/CSSvariables';
+import Preview_Place from '../../assets/Preview_Place_Large.png';
 
 class Give_Item_Add_Form extends Component {
   constructor(props) {
@@ -280,7 +290,7 @@ class Give_Item_Add_Form extends Component {
         console.log('giveItem is ' + giveItem_id);
       })
       .catch((err) => {
-        window.alert(err.response.data)
+        window.alert(err.response.data);
       });
 
     this.state.info.images.map(async (image) => {
@@ -295,22 +305,38 @@ class Give_Item_Add_Form extends Component {
         .catch((err) => window.alert(err.response.data));
     });
 
-  history.push("/top")
-  }
+    history.push('/top');
+  };
 
   render() {
     const { info, message, allCategory, allBland, imgUrls } = this.state;
     let mapImages;
     let deleteUploadedButton;
-    let alertMessage;
+    let previewPlace;
     if (imgUrls.length === 0) {
       mapImages = null;
-      alertMessage = <p>画像は最低一枚投稿してください。</p>;
+      previewPlace = (
+        <Preview_Place_Image src={Preview_Place} alc="プレビュー場所表示"></Preview_Place_Image>
+      );
     } else {
       mapImages = imgUrls.map((img, idx) => {
-        return <img key={idx} src={img} alc="アップロード写真" height="150px"></img>;
+        return <Preview_Image key={idx} src={img} alc="アップロード写真"></Preview_Image>;
       });
-      deleteUploadedButton = <button onClick={this.cancelUploadedImage}>画像取り消し</button>;
+      deleteUploadedButton = (
+        <DeleteButton onClick={this.cancelUploadedImage} imgUrls_length={imgUrls.length}>
+          画像取り消し
+        </DeleteButton>
+      );
+    }
+
+    let keywordError;
+
+    if (message.keyword1 != '') {
+      keywordError = message.keyword1;
+    } else if (message.keyword2 != '') {
+      keywordError = message.keyword2;
+    } else if (message.keyword3 != '') {
+      keywordError = message.keyword3;
     }
 
     // setStateが完了するまではnullにする。
@@ -318,124 +344,158 @@ class Give_Item_Add_Form extends Component {
       return <CircularProgress />;
     } else {
       return (
-        <div>
+        <div className={this.props.className}>
           <form onSubmit={this.handleSubmit}>
-            <div className="nameForm textForm">
-              <label>商品名</label>
-              <input name="name" type="text" value={info.name} onChange={this.handleChange} />
-              <p>{message.name}</p>
-            </div>
-
-            <div className="stateForm dropdownForm">
-              <label>状態</label>
-              <select onChange={this.handleChange} name="state">
-                <option value="新品">新品、未使用</option>
-                <option value="未使用">未使用に近い</option>
-                <option value="傷や汚れなし">目立った傷や汚れなし</option>
-                <option value="やや傷や汚れあり">やや傷や汚れあり</option>
-                <option value="傷や汚れあり">傷や汚れあり</option>
-                <option value="状態が悪い">全体的に状態が悪い</option>
-              </select>
-            </div>
-
-            <div className="keywordForm textForm">
-              {/* keywordのみValidationを一つに見せるため、上部に全てのメッセージを集約 */}
-              <p>{message.keyword1}</p>
-              <p>{message.keyword2}</p>
-              <p>{message.keyword3}</p>
-
-              <label>キーワード1</label>
-              <input
-                name="keyword1"
-                type="text"
-                value={info.keyword1}
-                onChange={this.handleChange}
+            <FormArea>
+              <TextLiTag>
+                <label>商品名</label>
+                <InputForm
+                  name="name"
+                  type="text"
+                  value={info.name}
+                  onChange={this.handleChange}
+                  placeholder="最大100字"
+                />
+              </TextLiTag>
+              <ValidationMessage
+                errorMessage={message.name}
+                isShowup={message.name != ''}
+                text_color="#D9F1FF"
+                margin="10px 0px 0px 140px"
+                bg_color="#70AACC"
               />
 
-              <label>キーワード2</label>
-              <input
-                name="keyword2"
-                type="text"
-                value={info.keyword2}
-                onChange={this.handleChange}
+              <OptionalLiTag>
+                <label>状態</label>
+                <DropDown onChange={this.handleChange} name="state">
+                  <option value="新品">新品、未使用</option>
+                  <option value="未使用">未使用に近い</option>
+                  <option value="傷や汚れなし">目立った傷や汚れなし</option>
+                  <option value="やや傷や汚れあり">やや傷や汚れあり</option>
+                  <option value="傷や汚れあり">傷や汚れあり</option>
+                  <option value="状態が悪い">全体的に状態が悪い</option>
+                </DropDown>
+              </OptionalLiTag>
+
+              <TextLiTag>
+                <label>キーワード1</label>
+                <InputForm
+                  name="keyword1"
+                  type="text"
+                  value={info.keyword1}
+                  onChange={this.handleChange}
+                  placeholder="最低1つ設定してください。"
+                />
+              </TextLiTag>
+
+              <TextLiTag>
+                <label>キーワード2</label>
+                <InputForm
+                  name="keyword2"
+                  type="text"
+                  value={info.keyword2}
+                  onChange={this.handleChange}
+                />
+              </TextLiTag>
+
+              <TextLiTag>
+                <label>キーワード3</label>
+                <InputForm
+                  name="keyword3"
+                  type="text"
+                  value={info.keyword3}
+                  onChange={this.handleChange}
+                />
+              </TextLiTag>
+              <ValidationMessage
+                errorMessage={keywordError}
+                isShowup={
+                  message.keyword1 != '' || message.keyword2 != '' || message.keyword3 != ''
+                }
+                text_color="#D9F1FF"
+                margin="10px 0px 0px 140px"
+                bg_color="#70AACC"
               />
 
-              <label>キーワード3</label>
-              <input
-                name="keyword3"
-                type="text"
-                value={info.keyword3}
-                onChange={this.handleChange}
+              <OptionalLiTag>
+                <label>ブランド</label>
+                <DropDown name="bland" onChange={this.handleChange}>
+                  <option value="">ブランド無し</option>
+                  {allBland.map((bland, idx) => {
+                    return (
+                      <option key={idx} value={bland.id}>
+                        {bland.name}
+                      </option>
+                    );
+                  })}
+                </DropDown>
+              </OptionalLiTag>
+
+              <TextLiTag>
+                <label>カテゴリ</label>
+                <DropDown name="category" onChange={this.handleChange}>
+                  <option value="">(必須項目)</option>
+                  {allCategory.map((category, idx) => {
+                    return (
+                      <option key={idx} value={category.id}>
+                        {category.name}
+                      </option>
+                    );
+                  })}
+                </DropDown>
+              </TextLiTag>
+              <ValidationMessage
+                errorMessage={message.category}
+                isShowup={message.category != ''}
+                text_color="#D9F1FF"
+                margin="10px 0px 0px 140px"
+                bg_color="#70AACC"
               />
-            </div>
 
-            <div className="blandForm dropdownForm">
-              <label>ブランド</label>
-              <select name="bland" onChange={this.handleChange}>
-                <option value="">ブランド無し</option>
-                {allBland.map((bland, idx) => {
-                  return (
-                    <option key={idx} value={bland.id}>
-                      {bland.name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
+              <ImageLiTag>
+                {/*  */}
+                <label>商品画像</label>
+                {/* Validation適用前から表示させたいためVaildationとは別に記述 */}
+                <InputFile type="file" multiple onChange={this.handleImageSelect} />
+                {previewPlace}
+                {mapImages}
+                {deleteUploadedButton}
+              </ImageLiTag>
+              <ValidationMessage
+                errorMessage={message.images}
+                isShowup={message.images != ''}
+                text_color="#FF737A"
+                margin="10px 0px 0px 140px"
+                bg_color="#FFBFC2"
+              />
 
-            <div className="categoryForm dropdownForm">
-              <label>カテゴリ</label>
-              <select name="category" onChange={this.handleChange}>
-                <option value="">---</option>
-                {allCategory.map((category, idx) => {
-                  return (
-                    <option key={idx} value={category.id}>
-                      {category.name}
-                    </option>
-                  );
-                })}
-              </select>
-              <p>{message.category}</p>
-            </div>
+              <TextLiTag>
+                <DetailLabel>説明</DetailLabel>
+                <StyledTextArea
+                  name="detail"
+                  value={info.detail}
+                  onChange={this.handleChange}
+                  placeholder="最大800字"
+                ></StyledTextArea>
+              </TextLiTag>
 
-            <div className="imageForm">
-              <p>{message.images}</p>
-              {/*  */}
-              <label>商品画像</label>
-              {/* Validation適用前から表示させたいためVaildationとは別に記述 */}
-              {alertMessage}
-              <input type="file" multiple onChange={this.handleImageSelect} />
-              {mapImages}
-              {deleteUploadedButton}
-            </div>
-
-            <div className="detailForm textarea">
-              <label>説明</label>
-              <textarea
-                name="detail"
-                cols="30"
-                rows="10"
-                value={info.detail}
-                onChange={this.handleChange}
-              ></textarea>
-            </div>
-
-            <MiddleButton
-              btn_name="登録"
-              btn_type="submit"
-              btn_disable={
-                !info.name ||
-                !info.keyword1 ||
-                info.images.length === 0 ||
-                !info.category ||
-                message.name ||
-                message.keyword1 ||
-                message.keyword2 ||
-                message.keyword3 ||
-                message.images ||
-                message.category
-              }
-            />
+              <MiddleButton
+                btn_name="登録"
+                btn_type="submit"
+                btn_disable={
+                  !info.name ||
+                  !info.keyword1 ||
+                  info.images.length === 0 ||
+                  !info.category ||
+                  message.name ||
+                  message.keyword1 ||
+                  message.keyword2 ||
+                  message.keyword3 ||
+                  message.images ||
+                  message.category
+                }
+              />
+            </FormArea>
           </form>
         </div>
       );
@@ -444,3 +504,83 @@ class Give_Item_Add_Form extends Component {
 }
 
 export default Give_Item_Add_Form;
+
+const FormArea = styled.ul`
+  label {
+    ${mixinUlLabel};
+    margin-right: 30px;
+    width: 110px;
+  }
+`;
+
+const TextLiTag = styled.li`
+  ${mixinLiTag};
+  margin-top: 15px;
+`;
+
+const OptionalLiTag = styled(TextLiTag)`
+  margin-bottom: 42px;
+`;
+
+const InputForm = styled.input`
+  ${mixinInputForm};
+`;
+
+const DropDown = styled.select`
+  ${mixinDropDown};
+  width: 200px;
+`;
+
+const DetailLabel = styled.label`
+  position: relative;
+  bottom: 90px;
+`;
+
+const StyledTextArea = styled.textarea`
+  ${mixinTextArea};
+`;
+
+const ImageLiTag = styled.li`
+  display: grid;
+  grid-template-columns: 157px 1fr 1fr 1fr 1fr 1fr;
+  margin-top: 15px;
+  grid-row-gap: 15px;
+  align-items: center;
+`;
+
+const InputFile = styled.input`
+  grid-column: 2 / 7;
+  width: 325px;
+`;
+
+const Preview_Image = styled.img`
+  width: 150px;
+  margin-right: 25px;
+`;
+
+const Preview_Place_Image = styled.img`
+  display: block;
+  height: 160px;
+  position: relative;
+  left: 128px;
+  grid-column: 1 / 7;
+`;
+
+const DeleteButton = styled.button`
+  grid-column: 1 / 7;
+  width: ${(props) => 160 * props.imgUrls_length}px;
+  position: relative;
+  left: 20px;
+  margin-top: 10px;
+  padding: 5px 10px;
+  border-radius: 5%;
+  font-size: 0.76rem;
+  color: #6c7880;
+  border: 1px solid #6c7880;
+  background: white;
+
+  &:hover {
+    color: white;
+    background: #6c7880;
+  }
+`;
--- frontend/src/containers/Organisms/User_Sidemenu.jsx
+++ frontend/src/containers/Organisms/User_Sidemenu.jsx
@@ -1,86 +1,85 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import history from '../../history';
-import { Colors } from "../../presentational/shared/static/CSSvariables";
+import { Colors } from '../../presentational/shared/static/CSSvariables';
 
 class User_Sidemenu extends Component {
- constructor(props){
- super(props);
-}
+  constructor(props) {
+    super(props);
+  }
 
- render() {
-     return (
-         <Wrapper>
-                <Menu>
-                    <li>
-                        <LinkText href={"/user/" + this.props.user_id}>ユーザー情報</LinkText>
-                    </li>
-                    {
-                        this.props.isUser &&
-                        <>
-                            <li>
-                                <LinkText href="/user/edit">アカウント情報の編集</LinkText>
-                            </li>
-                            <li>
-                                <LinkText href="/want/add">欲しいものリストの表示、追加</LinkText>
-                            </li>
-                            <li>
-                                <LinkText href="/give/add">商品の出品</LinkText>
-                            </li>
-                            <li>
-                                <LinkText href="/user/pickup">ピックアップリストの表示、追加</LinkText>
-                            </li>
-                        </>
-                    }
-                </Menu>
-         </Wrapper>
-     )
- }
+  render() {
+    return (
+      <Wrapper>
+        <Menu>
+          <li>
+            <LinkText href={'/user/' + this.props.user_id}>ユーザー情報</LinkText>
+          </li>
+          {this.props.isUser && (
+            <li>
+              <li>
+                <LinkText href="/user/edit">アカウント情報の編集</LinkText>
+              </li>
+              <li>
+                <LinkText href="/want/add">欲しいものリストの表示、追加</LinkText>
+              </li>
+              <li>
+                <LinkText href="/give/add">商品の出品</LinkText>
+              </li>
+              <li>
+                <LinkText href="/user/pickup">ピックアップリストの表示、追加</LinkText>
+              </li>
+            </li>
+          )}
+        </Menu>
+      </Wrapper>
+    );
+  }
 }
 
 export default User_Sidemenu;
 
 const Wrapper = styled.div`
-    width:300px;
-    padding-left: 25px;
-    padding-top: 25px;
-    border-right: solid 0.8px ${Colors.accent1};
-    z-index:29;
-    background:white;
+  width: 300px;
+  padding-left: 25px;
+  padding-top: 25px;
+  border-right: solid 0.8px ${Colors.accent1};
+  z-index: 29;
+  background: white;
 `;
 
 const Menu = styled.ul`
-    li{
-        margin-bottom:1.2rem;
-        color:${Colors.characters};
-        font-size:1.05rem;
-        font-weight:400;
-        list-style:none;
-    }
+  li {
+    margin-bottom: 1.2rem;
+    color: ${Colors.characters};
+    font-size: 1.05rem;
+    font-weight: 400;
+    list-style: none;
+  }
 `;
 
 const LinkText = styled.a`
-  text-decoration:none;
-  color:inherit;
-  position:relative;
+  text-decoration: none;
+  color: inherit;
+  position: relative;
 
-  &::after{
-      content:"";
-      position:absolute;
-      bottom:-2px;
-      left:0px;
-      width:100%;
-      height:2px;
-      background:${Colors.characters};
-      transform:scale(0,1);
-      transform-origin:left top;
-      transition:transform .3s;
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0px;
+    width: 100%;
+    height: 2px;
+    background: ${Colors.characters};
+    transform: scale(0, 1);
+    transform-origin: left top;
+    transition: transform 0.3s;
   }
 
-  &:hover{
-      font-weight:700;
+  &:hover {
+    font-weight: 700;
   }
-  &:hover::after{
-      transform:scale(1, 1);
+  &:hover::after {
+    transform: scale(1, 1);
   }
 `;
