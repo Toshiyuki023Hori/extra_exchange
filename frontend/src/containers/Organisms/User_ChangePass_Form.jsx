d670e611ac561aed6c5c003e563b555390b379f6
Toshi023 <World.88Toshi@gmail.com>
2020-10-23 15:05:42 +0900

    [Update] User_ChangePassレイアウト完成

--- frontend/src/containers/Organisms/User_ChangePass_Form.jsx
+++ frontend/src/containers/Organisms/User_ChangePass_Form.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import history from '../../history';
-import SmallButton from '../../presentational/shared/SmallButton';
+import MiddleButton from '../../presentational/shared/MiddleButton';
+import ValidationMessage from '../../presentational/shared/ValidationMessage';
+import {
+  Colors,
+  mixinInputForm,
+  mixinLiTag,
+  mixinUlLabel,
+} from '../../presentational/shared/static/CSSvariables';
 
 class User_ChangePass_Form extends Component {
   constructor(props) {
@@ -18,7 +25,6 @@ class User_ChangePass_Form extends Component {
         newPassword1: '',
         newPassword2: '',
       },
-      flashMessage: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -97,14 +103,14 @@ class User_ChangePass_Form extends Component {
       )
       .then((res) => {
         if (res.status == '200') {
-          this.setState({ flashMessage: 'パスワードの変更に成功しました。' });
+          window.alert('パスワードの変更に成功しました。');
         }
       })
-      .catch((err) =>
-        this.setState({
-          flashMessage: '変更に失敗しました。申し訳ありませんが、もう一度入力をお願いします。',
-        })
-      );
+      .catch((err) => {
+        if (err.response.status != '200') {
+          window.alert('変更に失敗しました。申し訳ありませんが、もう一度入力をお願いします。');
+        }
+      });
 
     this.setState({
       info: {
@@ -116,56 +122,118 @@ class User_ChangePass_Form extends Component {
   }
 
   render() {
-    const { flashMessage, info, message } = this.state;
+    const { resultMessage, info, message } = this.state;
     return (
-      <div>
-        <p>{flashMessage}</p>
-        <div>
-          <label>旧パスワード</label>
-          <input
-            onChange={this.handleChange}
-            type="password"
-            name="oldPassword"
-            value={info.oldPassword}
+      <div className={this.props.className}>
+        <h2>パスワード変更</h2>
+
+        <FormArea>
+          <StyledLiTag>
+            <label>旧パスワード</label>
+            <InputForm
+              onChange={this.handleChange}
+              type="password"
+              name="oldPassword"
+              value={info.oldPassword}
+            />
+          </StyledLiTag>
+          <ValidationMessage
+            errorMessage={message.oldPassword}
+            isShowup={message.oldPassword != ''}
+            text_color="#D9F1FF"
+            margin="10px 0px 0px 140px"
+            bg_color="#70AACC"
           />
-          {message.oldPassword}
-        </div>
-        <div>
-          <label>新パスワード</label>
-          <input
-            onChange={this.handleChange}
-            type="password"
-            name="newPassword1"
-            value={info.newPassword1}
+
+          <StyledLiTag>
+            <label>新パスワード</label>
+            <InputForm
+              onChange={this.handleChange}
+              type="password"
+              name="newPassword1"
+              value={info.newPassword1}
+            />
+          </StyledLiTag>
+          <ValidationMessage
+            errorMessage={message.newPassword1}
+            isShowup={message.newPassword1 != ''}
+            text_color="#D9F1FF"
+            margin="10px 0px 0px 140px"
+            bg_color="#70AACC"
           />
-          {message.newPassword1}
-        </div>
-        <div>
-          <label>新パスワード(確認用)</label>
-          <input
-            onChange={this.handleChange}
-            type="password"
-            name="newPassword2"
-            value={info.newPassword2}
+
+          <StyledLiTag>
+            <label>新パスワード(確認用)</label>
+            <InputForm
+              onChange={this.handleChange}
+              type="password"
+              name="newPassword2"
+              value={info.newPassword2}
+            />
+          </StyledLiTag>
+          <ValidationMessage
+            errorMessage={message.newPassword2}
+            isShowup={message.newPassword2 != ''}
+            text_color="#D9F1FF"
+            margin="10px 0px 0px 140px"
+            bg_color="#70AACC"
           />
-          {message.newPassword2}
-        </div>
-        <SmallButton
-          btn_name="変更"
-          btn_type="submit"
-          btn_click={this.handleSubmit}
-          btn_disable={
-            !info.oldPassword ||
-            !info.newPassword1 ||
-            !info.newPassword2 ||
-            message.oldPassword ||
-            message.newPassword1 ||
-            message.newPassword2
-          }
-        />
+
+          <StyledLiTag>
+            <SubmitButton
+              btn_type="submit"
+              btn_click={this.handleSubmit}
+              btn_disable={
+                !info.oldPassword ||
+                !info.newPassword1 ||
+                !info.newPassword2 ||
+                message.oldPassword ||
+                message.newPassword1 ||
+                message.newPassword2
+              }
+            >
+              変更
+            </SubmitButton>
+          </StyledLiTag>
+        </FormArea>
       </div>
     );
   }
 }
 
 export default User_ChangePass_Form;
+
+const FormArea = styled.ul`
+  label {
+    ${mixinUlLabel};
+    margin-right: 30px;
+    width: 160px;
+  }
+`;
+
+const StyledLiTag = styled.li`
+  ${mixinLiTag};
+  margin-top: 15px;
+`;
+
+const InputForm = styled.input`
+  ${mixinInputForm};
+`;
+
+const SubmitButton = styled(MiddleButton)`
+  display: block;
+  margin: 10px auto;
+  background: ${(props) => (!props.btn_disable ? '#8DD6FF' : '#E0F4FF')};
+  color: ${(props) => (!props.btn_disable ? '#466A80' : '#BDCFDA')};
+  box-shadow: 4px 3px ${Colors.accent1};
+
+  &:hover:enabled {
+    background-color: #a8e0ff;
+    transition: all 200ms linear;
+  }
+
+  &:active:enabled {
+    box-shadow: 0px 0px 0px;
+    transform: translate(4px, 3px);
+  }
+`;
--- frontend/src/containers/Pages/User_ChangePass.jsx
+++ frontend/src/containers/Pages/User_ChangePass.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 import { Redirect } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Header from '../Organisms/Header';
+import Footer from '../Organisms/Footer';
+import User_Sidemenu from '../Organisms/User_Sidemenu';
 import User_ChangePass_Form from '../Organisms/User_ChangePass_Form';
+import { mixinHeaderSpace } from '../../presentational/shared/static/CSSvariables';
 
 class User_ChangePass extends Component {
   constructor(props) {
@@ -33,10 +37,14 @@ class User_ChangePass extends Component {
       return (
         <>
           <Header loginUser={this.state.loginUser} />
-          <User_ChangePass_Form
-            loginUser={this.state.loginUser}
-            authUrl="http://localhost:8000/rest-auth/password/change/"
-          />
+          <Body>
+            <User_Sidemenu user_id={this.state.loginUser.id} isUser="true" />
+            <Styled_User_ChangePass_Form
+              loginUser={this.state.loginUser}
+              authUrl="http://localhost:8000/rest-auth/password/change/"
+            />
+          </Body>
+          <Footer />
         </>
       );
     }
@@ -44,3 +52,14 @@ class User_ChangePass extends Component {
 }
 
 export default User_ChangePass;
+
+const Body = styled.div`
+  ${mixinHeaderSpace};
+  display: flex;
+  height: 70vh;
+`;
+
+const Styled_User_ChangePass_Form = styled(User_ChangePass_Form)`
+  flex: 1;
+  padding: 20px 0px 0px 20px;
+`;
