8c33207dbe99d92f2cc3005c40243a60303f9aed
Toshi023 <World.88Toshi@gmail.com>
2020-10-25 12:48:22 +0900

    [Update] User_Descriptionレト完成

--- frontend/src/containers/Organisms/User_Description.jsx
+++ frontend/src/containers/Organisms/User_Description.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Want_Item_List from '../Organisms/Want_Item_List';
 import Icon from '../../presentational/shared/Icon';
+import { Colors } from '../../presentational/shared/static/CSSvariables';
 
 class User_Description extends Component {
   constructor(props) {
@@ -27,19 +28,37 @@ class User_Description extends Component {
       return null;
     } else {
       return (
-        <div>
-          <Icon icon={user.icon} img_width="70px" img_height="70px" img_radius="50%" />
-          <h1>{user.username}</h1>
+        <Wrapper>
+          <Icon_Div>
+            <Icon icon={user.icon} img_width="70px" img_height="70px" img_radius="50%" />
+            <h1>{user.username}</h1>
+          </Icon_Div>
           <Want_Item_List
             owner={user}
             loginUser={loginUser}
             h2Title={user.username + 'さんの欲しい物リスト'}
             axiosUrl="http://localhost:8000/api/"
+            margin_top='1.3rem'
           />
-        </div>
+        </Wrapper>
       );
     }
   }
 }
 
 export default User_Description;
+
+const Wrapper = styled.div`
+  margin-top:2rem;
+  border-top: 2px solid ${Colors.accent2};
+`;
+
+const Icon_Div = styled.div`
+  margin-top:2rem;
+  display:flex;
+  align-items:center;
+
+  h1{
+    margin-left:2rem;
+  }
+`;
